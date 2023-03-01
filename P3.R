78380b906ce666f75e079b2262858e7afc1410cd
fja15 <63888581+fja15@users.noreply.github.com>
2023-03-01 14:13:02 -0800

    Added code to train 3 to 16 states and plot results

--- P3.R
+++ P3.R
@@ -22,8 +22,32 @@ scaled_data <- subset(scaled_data, difftime(strptime(scaled_data$Time, format="%
 #unique(scaled_data$Date)
 
 set.seed(1)
-model <- depmix(response = scaled_data$Global_active_power ~ 1, data = scaled_data, nstates = 2, ntimes=c(52))
-fitModel <-fit(model)
-logLik(fitModel)
-BIC(fitModel)
-summary(fitModel)
+
+res_list <- array(c(0), dim = c(14,3))
+
+# Insert logLik and BIC values for each model and number of states into a 2d array
+for (i in 3:16) {
+
+  print(paste("nstates ", i, " run ", i-2))
+  
+  d <- weeks[[i]]
+  mod <- depmix(response = scaled_data$Global_active_power ~ 1, data = scaled_data, nstates = i, ntimes = rep(c(240), each=52))
+  
+  fm <- fit(mod)
+  
+  res_list[i-2, 1] = logLik(fm)
+  res_list[i-2, 2] = AIC(fm)
+  res_list[i-2, 3] = BIC(fm)
+  print(paste("logLik ", logLik(fm), " AIC ", AIC(fm), " BIC ", BIC(fm)))
+}
+
+print(res_list)
+
+d <- data.frame(res_list)
+names(d) = c("logLik", "AIC", "BIC")
+d$nstates = c(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+print(d)
+
+plot(unlist(d[1]), unlist(d[3]), xlab = "logLik", ylab = "BIC")
+plot(unlist(d[4]), unlist(d[1]), xlab = "nstates", ylab = "logLik")
+plot(unlist(d[4]), unlist(d[3]), xlab = "nstates", ylab = "BIC")
